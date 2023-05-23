9364f8542ff21c85f60e4ee2774e6fae0bd150a3
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-05-24 01:27:09 +0300

    replit

---
+++ src/main/java/daireninAlani/DaireninAlani.java
@@ -0,0 +1,27 @@
+package daireninAlani;
+
+public class DaireninAlani {
+    public static void main(String[] args) {
+ /*
+        Pi=3.14
+
+    Dairenin Cevresi : 2*Pi*r
+
+    Dairenin Alani : Pi*r*r
+
+    Ornek Cikti:  Cevresi : 43.96  ,  Alani: 153.86
+
+
+  */
+
+        int r=7;
+        double Pi=3.14;
+        System.out.println("Dairenin Cevresi : " + 2*Pi*r);
+        System.out.println("Dairenin Alani : " + Pi*r*r);
+
+
+
+
+
+    }
+}
