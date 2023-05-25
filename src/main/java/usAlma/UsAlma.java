dcd069b40b6d11628679c39da4bb434a10e71522
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-05-25 21:29:35 +0300

    ekleme

--- src/main/java/usAlma/UsAlma.java
+++ src/main/java/usAlma/UsAlma.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
 
     Scanner input = new Scanner(System.in);
     System.out.println("Lütfen sayıyı giriniz:");
+
     double sayi,us;
     sayi = input.nextDouble();//2
 
@@ -17,6 +18,14 @@ public static void main(String[] args) {
     System.out.println("Math.pow(sayi,us) = " + Math.pow(sayi,us));//8.0
 
 
+
+    //2. Yol
+    //Kullanıcıdan bir sayı alın ve sayının küpünün yarısını konsola yazdıran bir kod yazın.
+
+    System.out.println("Lutfen bir sayi giriniz");
+    double num= input.nextDouble();
+
+    System.out.println((num*num*num)/2);
 
 }
 
